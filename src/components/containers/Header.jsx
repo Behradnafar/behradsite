1e1c0720ca82f5bd30b8ca016d69df05aadcc15a
BehradNafarPorter <behrad.nafar@flyporter.com>
2021-12-29 11:35:28 -0500

    Added Header

---
+++ src/components/containers/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div className='d-flex align-items-end bg-black h-25 font-30 font-oswald font-weight-bold'>
+            <p className='text-custom-yellow ml-4 mr-3'>Welcome To</p>
+            <p className='text-white'>My Profile</p>
+        </div>
+     );
+}
+ 
+export default Header;
--- src/components/containers/Main.jsx
+++ src/components/containers/Main.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import Profile from './Profile';
 import Menu from './Menu';
+import Header from './Header';
 
 const Main = () => {
-    return ( 
-        <div className='d-flex flex-column main m-auto justify-content-between'>
-            <Profile/>
-            <Menu/>
+    return (
+        <div className='d-flex main m-auto justify-content-between'>
+            <div className='d-flex flex-column justify-content-between w-30'>
+                <Profile />
+                <Menu />
+            </div>
+            <div className='d-flex flex-column w-68'>
+                <Header/>
+            </div>
         </div>
-     );
+
+    );
 };
- 
+
 export default Main;
